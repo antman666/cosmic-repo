d2c6a517700610ced2cefe6995617dc1010df047
antman666 <945360554@qq.com>
2021-09-30 23:15:43 +0800

    update

---
+++ linux-cjktty/update.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+
+name = requests.get("https://archlinux.org/packages/search/json/?name=linux")
+name.raise_for_status()
+pkgver = json.loads(name.text).get("results")[0].get("pkgver")
+pkgrel = json.loads(name.text).get("results")[0].get("pkgrel")
+versions = pkgver[:4]
+
+with open("PKGBUILD") as f:
+    lists = f.readlines()
+
+for x in lists:
+    if "pkgver" in x:
+        num = lists.index(x)
+        break
+lists.remove(x)
+lists.insert(num, "pkgver=" + pkgver + "\n")
+for x in lists:
+    if "pkgrel" in x:
+        num = lists.index(x)
+        break
+lists.remove(x)
+lists.insert(num, "pkgrel=" + pkgrel + "\n")
+for x in lists:
+    if "version" in x:
+        num = lists.index(x)
+        break
+lists.remove(x)
+lists.insert(num, "_version=" + versions + "\n")
+
+with open("PKGBUILD","w") as f:
+    for i in lists:
+        f.write(i)
+
+print("done")
