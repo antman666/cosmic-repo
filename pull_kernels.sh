2a103bba8f0666a0ea42192f7bce3e4386f050e6
antman666 <945360554@qq.com>
2021-11-14 00:02:25 +0800

    update linux-xanmod-cacule* to linux-xanmod-tt*

--- pull_kernels.sh
+++ pull_kernels.sh
@@ -7,7 +7,7 @@ for i in ${urls[*]}; do
 		wget -q https://github.com/$i
 done
 
-urls=$(curl https://github.com/antman666/linux-xanmod-cacule-uksm-cjktty/releases/tag/linux-xanmod-cacule-uksm-cjktty_v5.14.15-1_gcc -s | grep "/antman666/linux-xanmod-cacule-uksm-cjktty/releases/download/linux-xanmod-cacule-uksm-cjktty_v5.14.15-1_gcc/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/antman666/linux-xanmod-tt-uksm-cjktty/releases/tag/linux-xanmod-tt-uksm-cjktty_v5.14.15-1_gcc -s | grep "/antman666/linux-xanmod-tt-uksm-cjktty/releases/download/linux-xanmod-tt-uksm-cjktty_v5.14.15-1_gcc/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
 		pkg=$(echo $i|awk -F '/' '{print$7}')
