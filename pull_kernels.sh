10ee7540b2beceb01350c98ccba424c72175e58b
antman666 <945360554@qq.com>
2021-12-18 18:29:38 +0800

    update repo

--- pull_kernels.sh
+++ pull_kernels.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-urls=$(curl https://github.com/antman666/linux-ck-uksm/releases/tag/linux-ck-uksm_v5.15.7-1 -s | grep "/antman666/linux-ck-uksm/releases/download/linux-ck-uksm_v5.15.7-1/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/antman666/linux-ck-uksm/releases/tag/linux-ck-uksm_v5.15.10-1 -s | grep "/antman666/linux-ck-uksm/releases/download/linux-ck-uksm_v5.15.10-1/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
 		pkg=$(echo $i|awk -F '/' '{print$7}')
@@ -7,7 +7,7 @@ for i in ${urls[*]}; do
 		wget -q https://github.com/$i
 done
 
-urls=$(curl https://github.com/antman666/linux-xanmod-tt-uksm-cjktty/releases/tag/linux-xanmod-tt-uksm-cjktty_v5.15.7-1_gcc -s | grep "/antman666/linux-xanmod-tt-uksm-cjktty/releases/download/linux-xanmod-tt-uksm-cjktty_v5.15.7-1_gcc/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/antman666/linux-xanmod-tt-uksm-cjktty/releases/tag/linux-xanmod-tt-uksm-cjktty_v5.15.10-1_gcc -s | grep "/antman666/linux-xanmod-tt-uksm-cjktty/releases/download/linux-xanmod-tt-uksm-cjktty_v5.15.10-1_gcc/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
 		pkg=$(echo $i|awk -F '/' '{print$7}')
