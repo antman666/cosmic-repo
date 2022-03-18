63b18b729eecfdb9bffd22f8189fd63cf554d696
antman666 <945360554@qq.com>
2022-03-18 22:41:21 +0800

    update linux-ck-uksm-cjktty to 5.16.15-1; linux-ck-uksm to 5.16.15-1; linux-xanmod-tt-uksm-cjktty to 5.15.29-1

--- pull_kernels.sh
+++ pull_kernels.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-urls=$(curl https://github.com/antman666/linux-ck-uksm/releases/tag/linux-ck-uksm_v5.16.14-1 -s | grep "/antman666/linux-ck-uksm/releases/download/linux-ck-uksm_v5.16.14-1/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/antman666/linux-ck-uksm/releases/tag/linux-ck-uksm_v5.16.15-1 -s | grep "/antman666/linux-ck-uksm/releases/download/linux-ck-uksm_v5.16.15-1/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
 		pkg=$(echo $i|awk -F '/' '{print$7}')
@@ -7,7 +7,7 @@ for i in ${urls[*]}; do
 		wget -q https://github.com/$i
 done
 
-urls=$(curl https://github.com/antman666/linux-xanmod-tt-uksm-cjktty/releases/tag/linux-xanmod-tt-uksm-cjktty_v5.15.27-1 -s | grep "/antman666/linux-xanmod-tt-uksm-cjktty/releases/download/linux-xanmod-tt-uksm-cjktty_v5.15.27-1/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/antman666/linux-xanmod-tt-uksm-cjktty/releases/tag/linux-xanmod-tt-uksm-cjktty_v5.15.29-1 -s | grep "/antman666/linux-xanmod-tt-uksm-cjktty/releases/download/linux-xanmod-tt-uksm-cjktty_v5.15.29-1/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
 		pkg=$(echo $i|awk -F '/' '{print$7}')
