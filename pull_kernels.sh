28261459401b7d36a4bd882f14e4cf47bc99b66e
antman666 <945360554@qq.com>
2022-03-19 21:38:47 +0800

    update linux-ck-uksm to 5.16.15-2; linux-ck-uksm-cjktty to 5.16.15-2

--- pull_kernels.sh
+++ pull_kernels.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-urls=$(curl https://github.com/antman666/linux-ck-uksm/releases/tag/linux-ck-uksm_v5.16.15-1 -s | grep "/antman666/linux-ck-uksm/releases/download/linux-ck-uksm_v5.16.15-1/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://github.com/antman666/linux-ck-uksm/releases/tag/linux-ck-uksm_v5.16.15-2 -s | grep "/antman666/linux-ck-uksm/releases/download/linux-ck-uksm_v5.16.15-2/"|awk -F '"|"' '{print$2}')
 
 for i in ${urls[*]}; do 
 		pkg=$(echo $i|awk -F '/' '{print$7}')
