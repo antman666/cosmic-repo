e5814062fa116f08eaef97190cf20d87dd8f550e
antman666 <945360554@qq.com>
2023-01-02 22:59:23 +0800

    update linux-xanmod-tt-uksm-cjktty to 6.1.2-1

--- pull_kernels.sh
+++ pull_kernels.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-urls=$(curl https://github.com/antman666/linux-xanmod-tt-uksm-cjktty/releases/tag/linux-xanmod-tt-uksm-cjktty_v5.15.54-1 -s | grep "/antman666/linux-xanmod-tt-uksm-cjktty/releases/download/linux-xanmod-tt-uksm-cjktty_v5.15.54-1/"|awk -F '"|"' '{print$2}')
+urls=$(curl https://api.github.com/repos/antman666/linux-xanmod-tt-uksm-cjktty/releases/latest -s | grep browser_download_url | awk '{print $2}')
 
 for i in ${urls[*]}; do 
-		pkg=$(echo $i|awk -F '/' '{print$7}')
+		pkg=$(echo $i | awk -F '/' '{print $9}')
 		echo Downloading $pkg
 		wget -q https://github.com/$i
 done
