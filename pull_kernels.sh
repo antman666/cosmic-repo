4ca7617f554450c18f5bce15ca09f86607162073
antman666 <62091572+antman666@users.noreply.github.com>
2024-08-11 22:27:21 +0800

    Delete pull_kernels.sh

--- pull_kernels.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-urls=$(curl https://api.github.com/repos/antman666/linux-xanmod-tt-uksm-cjktty/releases/latest -s | grep browser_download_url | awk '{print $2}')
-
-for i in ${urls[*]}; do 
-		pkg=$(echo $i | awk -F '/' '{print $9}')
-		echo Downloading ${pkg%?}
-		wget -q ${i//\"/}
-done
